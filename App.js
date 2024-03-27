ba25143296c81bfb093c49b31f54ebe931d8d8c9
wczy-ao <wanzhi886@163.com>
2024-03-27 20:36:13 +0800

    TabBar式导航

--- App.js
+++ App.js
@@ -1,70 +1,51 @@
-import "react-native-gesture-handler";
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { Ionicons,AntDesign } from "@expo/vector-icons";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
-import CategoriesScreen from './screens/CategoriesScreen';
-import MealsOverviewScreen from './screens/MealsOverviewScreen';
-import MealDetailScreen from './screens/MealDetailScreen';
-import FavoritesScreen from './screens/FavoritesScreen';
+import Test1 from './screens/Test';
+import Test2 from './screens/Test2';
+import Test3 from './screens/Test3';
 
 const Drawer = createDrawerNavigator();
 const Stack = createNativeStackNavigator();
 
-function DrawerNavigator() {
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: '#351401' },
-        headerTintColor: 'white',
-        sceneContainerStyle: { backgroundColor: '#3f2f25' },
-      }}
-    >
-      <Drawer.Screen
-        name="Categories"
-        component={CategoriesScreen}
-        options={{
-          title: 'All Categories',
-        }}
-      />
-      <Drawer.Screen name="Favorites" component={FavoritesScreen} />
-    </Drawer.Navigator>
-  );
-}
+const Tab = createBottomTabNavigator();
+
+
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator
+        <Tab.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: '#351401' },
             headerTintColor: 'white',
             contentStyle: { backgroundColor: '#3f2f25' },
           }}
         >
-          <Stack.Screen
-            name="Drawer"
-            component={DrawerNavigator}
+          <Tab.Screen
+            name="首页"
+            component={Test1}
             options={{
               headerShown: false,
             }}
           />
-          <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
-          <Stack.Screen
-            name="MealDetail"
-            component={MealDetailScreen}
+          <Tab.Screen name="推荐" component={Test2} />
+          <Tab.Screen
+            name="个人"
+            component={Test3}
             options={{
               title: 'About the Meal',
             }}
           />
-        </Stack.Navigator>
+        </Tab.Navigator>
       </NavigationContainer>
     </>
   );
---
+++ screens/Test3.js
@@ -0,0 +1,16 @@
+import { Text, View, StyleSheet } from "react-native";
+
+const Test3 = () => {
+  return (
+    <View style={Styles.container}>
+      <Text>Test3</Text>
+    </View>
+  );
+};
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+export default Test3;
